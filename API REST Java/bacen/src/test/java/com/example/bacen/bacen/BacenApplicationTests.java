15b945f7678155cb2fc9ecc9bd7d7845a0bb5c5e
Matheus Valero <matheusvalero99@gmail.com>
2024-06-13 21:17:30 -0300

    test commit

---
+++ API REST Java/bacen/src/test/java/com/example/bacen/bacen/BacenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bacen.bacen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BacenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
